83494b8b1b23c25f34cd273b1e181576392893bb
saumya14 <saumya14@gmail.com>
2012-09-15 18:16:06 +0530

    Test file created, changes made to biblioteca.java

--- src/biblioteca.java
+++ src/biblioteca.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 /**
  * Created with IntelliJ IDEA.
  * User: user
@@ -9,38 +14,72 @@ public class biblioteca {
     public static final int RESERVED=1;
     public static final int AVAILABLE=0;
 
-    class Book{
-       String bookName;
-       String authorName;
-       Integer status;
-
-        Book()
-        {
-            bookName="";
-            authorName="";
-            status=AVAILABLE;
-        }
-        Book(String bookName,String authorName)
-        {
-            this.bookName=bookName;
-            this.authorName=authorName;
-            status=AVAILABLE;
-        }
-        void displayBooks()
-        {
-            System.out.print(bookName+"\t"+authorName+"\t"+status);
-        }
-        void reserveBook(String bookName)
-        {
-
-        }
-
-
-   }
-
-    public static void main( String args[])
+    ArrayList listOfBooks;
+    biblioteca()
     {
-        System.out.println("hello world");
+        listOfBooks = new ArrayList();
+    }
+
+    String displayWelcomeMessage()
+    {
+        return "--------Hi Member. Welcome to Bangalore Biblioteca--------";
+    }
+
+    void displayAllBooks()
+    {
+
+    }
+
+    void reserveBook()
+    {
+
+    }
+
+    void viewDetails()
+    {
+
+
+    }
+    void displayOptions()  throws IOException
+    {
+        BufferedReader dataInput=new BufferedReader(new InputStreamReader(System.in)) ;
+        int intChoice;
+        String choice;
+
+        System.out.println("Please choose what you want to do from the list given below: ");
+        System.out.println("1->See all the books in the library");
+        System.out.println("2->Reserve a book") ;
+        System.out.println("3->Check your details");
+
+        do{
+            System.out.println("Please enter a valid choice:");
+            choice= dataInput.readLine();
+            intChoice=Integer.parseInt(choice);
+        }while(intChoice!=1 && intChoice!=2 && intChoice!=3);
+
+
+        switch(intChoice)
+        {
+            case 1: displayAllBooks();
+                    break;
+            case 2: reserveBook();
+                    break;
+            case 3: viewDetails();
+                    break;
+
+        }
+
+
+    }
+
+
+
+    public static void main( String args[]) throws IOException
+    {
+        biblioteca Biblioteca=new biblioteca();
+        System.out.println(Biblioteca.displayWelcomeMessage());
+        Biblioteca.displayOptions();
+
     }
 
 }
---
+++ test/TestBiblioteca.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: user
+ * Date: 9/15/12
+ * Time: 5:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestBiblioteca {
+    @Test
+    public void testDisplayWelcomeMessage()
+    {
+        biblioteca bTest=new biblioteca();
+        assertTrue(bTest.displayWelcomeMessage().equals("--------Hi Member. Welcome to Bangalore Biblioteca--------"));
+
+    }
+
+}
